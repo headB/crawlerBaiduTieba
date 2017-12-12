2172d6ec3893a3a0c19b23c3070d03df60a57914
beetle <775121173@qq.com>
2017-12-12 17:16:20 +0800

    这个是闭包的形式，还包含了其他的类的类似的方式。

--- 进程池实例.py
+++ 进程池实例.py
@@ -1,10 +1,13 @@
 #-*- coding: utf-8 -*-
-import multiprocessing
-from multiprocessing import Pool
+#假如程序中已经用了“进程池”，就不要导入queue了，导入Manger？？？的queue
+from multiprocessing import Pool,Manager
 import time
+
+
 
 #这里设置一个变量，用来模拟假如每一个进程每下载好一次图片，就登记一下，然后累计。
 downloadTime = 0
+#duilie = Manager().Queue()
 
 def testPrint(xx):
     time.sleep(2)
@@ -17,9 +20,22 @@ class plan:
         
         #请勿在运行多进程的函数里面操作前面已经有的全局变量，一操作的话，这个函数感觉就不会运行了。
         # downloadTime += 1
+        if not duilie.full():
+            pass
+            #duilie.put_nowait("+1")
         time.sleep(2)
         print("I am engine!" )
         #print(downloadTime)
+        
+    def landingGear(self):
+        print("I am landingGear!!")
+        return self
+    
+    def wingStatus(self):
+        print("the wing angle is 60*")
+        
+    def testInstanceBibao(self):
+        print("I maybe a BiBao!")
         
     def testInstance(self):
         
@@ -70,11 +86,30 @@ if __name__ == "__main__":
             pool3.apply_async(callInstanceMethod,)
     pool3.close()
     
+    #测试一个东西很像闭包的东西
+    #但是这个是对象来的
+    B.landingGear().wingStatus()
     
     #下面这个join，假如不加入join的话，下面的print（“就会首先运行了，因为这个就是多进程的概念，自己管自己。”）
     #pool3.join()
     #print("run me ???")
     
+    ##这里就测试一个闭包，复习一下。
+    def test1():
+        name = "kumanxuan"
+        print(name)
+        def test2():
+            name2 = "lizhixuan"
+            print(name2)
+            return name
+        return test2
+    
+    ##调用闭包函数
+    test1()()
+    
+    ##调用类的类似闭包先。
+    
+    plan().testInstanceBibao()
     
 #pool = Pool(2)
 #for x in range(8):
