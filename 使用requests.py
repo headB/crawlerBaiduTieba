fcb5c1711bb7fee899a744f2801837ac1bccf810
beetle <775121173@qq.com>
2017-12-17 23:34:02 +0800

    Update 使用requests.py

--- 使用requests.py
+++ 使用requests.py
@@ -10,14 +10,57 @@ import requests
 
 #添加浏览器身份
 
-UA = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063"}
+#添加浏览器身份
+
+##输入你想查询的单词
+
+queryWord = raw_input("请输入你想查询的单词:")
+
+UA = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063",
+            "Accept":"application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding":"gzip, deflate",
+            "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8",
+            "Connection":"keep-alive",
+            "Cache-Control":"no-cache",
+            "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
+            "Cookie":"OUTFOX_SEARCH_USER_ID=-110@1010.168.8.22;JSESSIONID=aaab_t1HoLozmh8VH0xbw;",
+            "Host":"fanyi.youdao.com",
+            "Origin":"http://fanyi.youdao.com",
+            "Referer":"http://fanyi.youdao.com/",
+            "X-Requested-With":"XMLHttpRequest"
+         }
 #response = requests.get("http://www.baidu.com")
 #不添加浏览器身份就乱码输出？？？真是奇了个怪。
-response = requests.get("http://www.baidu.com",headers=UA)
 
 
-print(type(response))
+#设置post信息。
 
-#print(type(response.text))
-#打印得知，这个是unicode，意味着，我可以转换成utf8，尝试一下先。
+
+cryptKey1 = "aNPG!!u6sesA>hBAW1@(-"
+cryptKey2 = "fanyideskweb"
+thisTime = int(time.time())*1000
+str1 = cryptKey2+queryWord+str(thisTime)+cryptKey1
+cryptStr = md5(str1).hexdigest()
+
+
+post_json = {  
+            "i":queryWord,
+            "from":"AUTO",
+            "to":"AUTO",
+            "smartresult":"dict",
+            "client":"fanyideskweb",
+            "salt":thisTime,
+            "sign":cryptStr,      
+            "doctype":"json",
+            "version":"2.1",
+            "keyfrom":"fanyi.web",
+            "action":"FY_BY_REALTIME",
+            "typoResult":"false",
+                    }
+url = "http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+response = requests.post(url=url,headers=UA,data=post_json)
+
 print(response.text)
+
+
