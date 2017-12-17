902dcbd4f5503159a58b781b20bfe37ed6735579
beetle <775121173@qq.com>
2017-12-17 22:58:40 +0800

    添加了header信息才没有乱码。

--- 使用requests.py
+++ 使用requests.py
@@ -8,6 +8,16 @@ import time
 from hashlib import md5
 import requests
 
-response = requests.get("http://www.baidu.com")
+#添加浏览器身份
+
+UA = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063"}
+#response = requests.get("http://www.baidu.com")
+#不添加浏览器身份就乱码输出？？？真是奇了个怪。
+response = requests.get("http://www.baidu.com",headers=UA)
+
+
 print(type(response))
+
+#print(type(response.text))
+#打印得知，这个是unicode，意味着，我可以转换成utf8，尝试一下先。
 print(response.text)
