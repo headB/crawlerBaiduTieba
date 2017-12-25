0ac2119bd06f4c2f5650140e2c180f9f6bef9e4a
beetle <775121173@qq.com>
2017-12-25 14:45:09 +0800

    成功爬取一个淘宝的完整页啦，哈哈哈，准备去尝试京东的。

--- 用selenium模拟打开淘宝并滚动屏幕.py
+++ 用selenium模拟打开淘宝并滚动屏幕.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.PhantomJS(executable_path="/home/kumanxuan/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
-#driver.get("https://www.taobao.com")
+driver.get("https://www.taobao.com")
 #driver.get("https://jd.com")
-driver.get("http://www.gdcvi.net")
+#driver.get("http://www.gdcvi.net")
 ##不要这么逗好吗？不设置宽度，你还真是不按套路走
 
 driver.set_window_size(width=1920,height=1080)
@@ -18,18 +18,23 @@ js2 = "document.body.scrollTop=1080"
 time.sleep(1)
 
 #组合拳
-#for x in range(10):
-#    driver.execute_script(js2)
-#    time.sleep(2)
+height = 1080
+for x in range(8):
+    jsSQL = "document.body.scrollTop="+str(height)
+    driver.execute_script(jsSQL)
+    print(height)
+    height += 1080
+    time.sleep(1)
 
 #driver.execute_script(js)
 #time.sleep(6)
 
 #这些语句都是能爬，但是关键的是，部分细节还是没有爬出来啊。~只能说，现在爬一个大概吧。
-js3 = "window.scrollTo(0,document.body.scrollHeight)"
+##下面这些语句不知道怎么爬。大概意思应该是全爬，但问题是，刚刚爬淘宝的效果比我上面的爬的结果还要更加差点。
+#js3 = "window.scrollTo(0,document.body.scrollHeight)"
 
-driver.execute_script(js3)
-time.sleep(2)
+#driver.execute_script(js3)
+#time.sleep(2)
 
 
 driver.save_screenshot("taobao.png")
