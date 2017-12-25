76ec502a3cedd59e10a0f75bf9ab668a0f05403e
beetle <775121173@qq.com>
2017-12-25 14:10:42 +0800

    修改了url，变成去爬京东了。

--- 用selenium模拟打开淘宝并滚动屏幕.py
+++ 用selenium模拟打开淘宝并滚动屏幕.py
@@ -4,22 +4,31 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.PhantomJS(executable_path="/home/kumanxuan/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
-driver.get("https://www.taobao.com")
-
+#driver.get("https://www.taobao.com")
+driver.get("https://jd.com")
 ##不要这么逗好吗？不设置宽度，你还真是不按套路走
 
 driver.set_window_size(width=1920,height=1080)
 
 # 向下滚动1080*2像素,刚好一页
-js = "document.body.scrollTop=1080"
+js = "document.body.scrollTop=30000"
+js2 = "document.body.scrollTop=1080"
 
 time.sleep(1)
 
 #组合拳
-for x in range(5):
-    driver.execute_script(js)
-    time.sleep(1)
+#for x in range(10):
+#    driver.execute_script(js2)
+#    time.sleep(2)
 
+#driver.execute_script(js)
+#time.sleep(6)
+
+#这些语句都是能爬，但是关键的是，部分细节还是没有爬出来啊。~只能说，现在爬一个大概吧。
+js3 = "window.scrollTo(0,document.body.scrollHeight)"
+
+driver.execute_script(js3)
+time.sleep(2)
 
 
 driver.save_screenshot("taobao.png")
