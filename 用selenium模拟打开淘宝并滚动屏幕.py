9287e9a2a1f43212955b6f1006882f7f96282a49
beetle <775121173@qq.com>
2017-12-25 11:39:38 +0800

    用selenium模拟打开淘宝并滚动屏幕

---
+++ 用selenium模拟打开淘宝并滚动屏幕.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.PhantomJS(executable_path="/home/kumanxuan/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
+driver.get("https://www.taobao.com")
+
+##不要这么逗好吗？不设置宽度，你还真是不按套路走
+
+driver.set_window_size(width=1920,height=1080)
+
+# 向下滚动1080*2像素,刚好一页
+js = "document.body.scrollTop=1080"
+
+time.sleep(1)
+
+#组合拳
+for x in range(5):
+    driver.execute_script(js)
+    time.sleep(1)
+
+
+
+driver.save_screenshot("taobao.png")
