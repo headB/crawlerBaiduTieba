c073800fcd4ec3ae74a977d6617edb4301041f50
beetle <775121173@qq.com>
2017-12-27 09:47:13 +0800

    可以直接刷新查看新结果，但是发现准确率还是比较低的。

--- (pytesseract)测试验证码.py
+++ (pytesseract)测试验证码.py
@@ -1,7 +1,17 @@
 import pytesseract
 from PIL import Image
 
-image = Image.open('image.png')
+##动态去捉取老师评价网站的验证码并下载下来保存到本地
+
+url = "http://192.168.113.2/form/image.php"
+
+response = urllib2.urlopen(url).read()
+
+with open('imagesGet.png','w') as file:
+    
+    file.write(response)
+
+image = Image.open('imagesGet.png')
 
 text = pytesseract.image_to_string(image)
 
