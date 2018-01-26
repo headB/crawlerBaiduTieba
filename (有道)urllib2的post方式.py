068ecf1dc50638db13cca69a53f63c1aa9655afc
kuman-lenovo <775121173@qq.com>
2018-01-27 00:24:46 +0800

    原来有道这个代码里面,一直缺少前部的导入文件,哎呀,我真的不知道啊.!!

--- (有道)urllib2的post方式.py
+++ (有道)urllib2的post方式.py
@@ -1,3 +1,10 @@
+import urllib,urllib2
+from lxml import etree
+from gzip import GzipFile
+from StringIO import StringIO
+import datetime
+import time
+from hashlib import md5
 class fanyi:
     
     
